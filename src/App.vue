5dc316573031dc689d9212143184f36e9de3d9c7
Raymundosalas <128091595+Raymundosalas@users.noreply.github.com>
2026-02-25 11:30:59 -0600

    Add App.vue with Header and Navbar components

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+    <Navbar />
+    <main class="container">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+import Navbar from './components/Navbar.vue'
+</script>
