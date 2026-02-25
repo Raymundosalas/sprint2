483225dfa116185d77816acb7e24192b578e4b6f
Raymundosalas <128091595+Raymundosalas@users.noreply.github.com>
2026-02-25 11:30:31 -0600

    Add Navbar component with navigation links

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar">
+    <ul>
+      <li><router-link to="/">Inicio</router-link></li>
+      <li><router-link to="/pacientes">Pacientes</router-link></li>
+      <li><a href="#">Citas</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  background-color: #34495e;
+  padding: 10px;
+}
+
+.navbar ul {
+  list-style: none;
+  display: flex;
+  gap: 20px;
+}
+
+.navbar a {
+  color: white;
+  text-decoration: none;
+}
+</style>
